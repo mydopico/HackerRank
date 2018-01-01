8bfa2dfa1c03da489eb2b0124a221dd24a77e72e
mydopico <mydopico@gmail.com>
2018-01-01 19:35:36 +0100

    Update arithmetic_operators.py

--- Introduction/arithmetic_operators.py
+++ Introduction/arithmetic_operators.py
@@ -1,17 +1,32 @@
-Task
-Read two integers from STDIN and print three lines where:
+# Task
+# Read two integers from STDIN and print three lines where:
 
-    The first line contains the sum of the two numbers.
-    The second line contains the difference of the two numbers (first - second).
-    The third line contains the product of the two numbers.
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+# Input Format
+# The first line contains the first integer, aa. The second line contains the second integer, bb.
 
-Input Format
+# Constraints
+# 1≤a≤10101≤a≤1010
+# 1≤b≤10101≤b≤1010
 
-The first line contains the first integer, a. The second line contains the second integer, b. 
+# Output Format
+# Print the three lines as explained above.
 
-Output Format
+# Sample Input
 
-Print the three lines as explained above. 
+# 3
+# 2
+# Sample Output
+
+# 5
+# 1
+# 6
+# Explanation
+# 3+2⟹53+2⟹5
+# 3−2⟹13−2⟹1
+# 3∗2⟹6
 
 a = int(raw_input())
 b = int(raw_input())
