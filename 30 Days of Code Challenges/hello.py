f43ad91ac7aa360d6ee50eb41bc0a55c31b0e769
mydopico <mydopico@gmail.com>
2018-01-07 19:47:52 +0000

    Create hello.py

---
+++ 30 Days of Code Challenges/hello.py
@@ -0,0 +1,9 @@
+# To complete this challenge, you must save a line of input from stdin to a variable, 
+# print Hello, World. on a single line, and finally print the value of your variable 
+# on a second line.
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+inputString = raw_input() # ex. Welcome to 30 Days of Code!
+
+# Print a string literal saying "Hello, World." to stdout.
+print 'Hello, World.' + "\n" + inputString
